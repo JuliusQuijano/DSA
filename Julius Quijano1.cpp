09499adb68a7321c5e242930de005e4e3e3cf3e4
juliusQuijano <quijanojulius2345@.gmail.com>
2025-02-15 11:39:08 +0800

    pogiko

---
+++ Julius Quijano1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int myArray[30];
+int numElem = 0;
+int maxElem = 20;
+
+void traverseArray(){
+    cout << "Showing elements in my array\n";
+    for(int i= 0; i< maxElem; i++){
+        cout<< to_string(myArray[i]) + " | ";
+    }
+    cout << "\n";
+    
+}
+
+
+int main(){
+ myArray[0] = 0;
+ myArray[1] = 1;
+ myArray[2] = 2;
+ myArray[3] = 3;
+ myArray[4] = 4;
+
+ 
+
+traverseArray();
+
+
+
+    
+
+
+
+return 0;
+ 
+
+}
